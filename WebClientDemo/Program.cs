ff383ce963cbbaa8cb9f71b5172c3734326a3723
Elitsa Miroslavova Marinovska <elit0452@edu.eal.dk>
2017-09-07 11:27:33 +0200

    ex.3

---
+++ WebClientDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            WebClient client = new WebClient();
+            client.Headers.Add("Content-Type", "text/plain; charset=UTF-8");
+            client.Encoding = Encoding.UTF8;
+
+            string response = client.DownloadString("http://webservicedemo.datamatiker-skolen.dk/RegneWcfService.svc/RESTjson/Add?a=" + int.Parse(Console.ReadLine()) + "&b=" + int.Parse(Console.ReadLine()));
+            Console.WriteLine("The result is " + response);
+
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
